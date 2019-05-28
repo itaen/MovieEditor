a0c2cbb1b633f5ed6e7cf3e371a4c847c15f6a1f
Tank <417455771@qq.com>
2019-05-28 16:58:29 +0800

    init project

---
+++ MovieEditorBuilder/MovieEditorBuilder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MovieEditorBuilder
+//
+//  Created by itaen on 2019/5/28.
+//  Copyright © 2019 itaen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MovieEditorBuilder/MovieEditorBuilder/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MovieEditorBuilder
+//
+//  Created by itaen on 2019/5/28.
+//  Copyright © 2019 itaen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+
+@end
